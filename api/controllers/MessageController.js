02768f946160fab7ea417818f46e7a85b77066e2
aaronrussell <aaron@teknologenie.com>
2015-03-25 18:43:39 -0700

    added check for insuring a user inserts a message and has an ID

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -8,13 +8,21 @@
 module.exports = {
 
     send: function(req, res) {
-        Message.create({ message: req.body.message, user: req.body.userId}).exec(function aftwards(err, message) {
-            if (message) {
-                res.json(message);
-            } else if (err) {
-                res.json({ error: 'Could not send message' }, 404);
-            }
-        });
+        if(!req.body.message) {
+            return res.serverError({ error: 'You need to type a message in', errorCode: 'MESSAGE_REQUIRED' }, 404)
+        } else if(!req.body.user) {
+            return res.serverError({ error: 'You dont have a user ID stop trying to hack us', errorCode: 'NO_USERID' }, 404)
+
+        } else {
+            Message.create({ message: req.body.message, user: req.body.userId}).exec(function aftwards(err, message) {
+                if (message) {
+                    res.json(message);
+                } else if (err) {
+                    res.json({ error: 'Could not send message' }, 404);
+                }
+            });
+        }
+
     }
 	
 };
